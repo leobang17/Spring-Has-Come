36a3e6479a9a513429eb6812c80ae27b11af34a5
LeoBang <bluke8489@gmail.com>
2022-03-07 18:41:56 +0900

    servlet

--- Servlet/src/main/java/hello/Servlet/ServletInitializer.java
+++ Servlet/src/main/java/hello/Servlet/ServletInitializer.java
@@ -1,4 +1,4 @@
-package hello.Servlet;
+package hello.servlet;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
