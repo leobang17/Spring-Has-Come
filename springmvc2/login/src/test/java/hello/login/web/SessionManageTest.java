a88baedc83c67819f628c15b8f2307c93f50fcfc
LeoBang <bluke8489@gmail.com>
2022-03-18 21:47:57 +0900

    mvc2 ~ login1

---
+++ springmvc2/login/src/test/java/hello/login/web/SessionManageTest.java
@@ -0,0 +1,35 @@
+package hello.login.web;
+
+import hello.login.domain.member.Member;
+import hello.login.web.session.SessionManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class SessionManageTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        // 세션 생성
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        // 요청에 응답 쿠키 저장 (mock 웹브라우저의 요청, 응답)
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        // 세션 조회
+        Object result = sessionManager.getSession(request);
+        Assertions.assertThat(result).isEqualTo(member);
+
+        // 세션 만료
+        sessionManager.expire(request);
+        Object expired = sessionManager.getSession(request);
+        Assertions.assertThat(expired).isNull();
+    }
+}
